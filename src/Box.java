7672a5767da448fd007b96ce7704f82d3c280526
Ksenia <ksyshishe@mail.ru>
2023-12-27 00:50:00 +0300

    it's not work

--- src/Box.java
+++ src/Box.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Box extends JPanel {
 
@@ -10,8 +12,12 @@ public class Box extends JPanel {
         cell = new Cell();
         setBounds(x * Config.SIZE, y * Config.SIZE, Config.SIZE, Config.SIZE);
         setBackground(Config.getColor(Status.NONE));
-    }
-
+        /*addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                cell.turn();
+            }*/
+        }
     public void initCells(int x, int y) {
 
     }
