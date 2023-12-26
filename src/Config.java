80256cbedee60f18be1f3012e9ad8233cca0b681
Ksenia <ksyshishe@mail.ru>
2023-12-27 00:35:17 +0300

    //

--- src/Config.java
+++ src/Config.java
@@ -4,6 +4,7 @@ public class Config {
     public static final int SIZE = 20;
     public static final int WIDTH = 40;
     public static final int HEIGHT = 30;
+    public static final int SLEEPMS = 500;
 
     public static Color getColor (Status status){
         return switch (status) {
--- src/Window.java
+++ src/Window.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Window implements Runnable {
 
@@ -24,28 +26,48 @@ public class Window implements Runnable {
 
     void initBoxes() {
         boxes = new Box[Config.WIDTH][Config.HEIGHT];
-        for (int x = 0; x < Config.WIDTH; x ++)
-            for (int y = 0; y < Config.HEIGHT; y ++){
-                boxes [x][y] = new Box (x,y);
+        for (int x = 0; x < Config.WIDTH; x++)
+            for (int y = 0; y < Config.HEIGHT; y++) {
+                boxes[x][y] = new Box(x, y);
                 frame.add(boxes[x][y]);
             }
 
-        for (int x = 0; x < Config.WIDTH; x ++) //суть: выбираем конкретную клетку на экране,
-            for (int y = 0; y < Config.HEIGHT; y ++){
-                for (int sx = -1; sx <= +1; sx ++)  //вокруг этой клетки проверяем 8 клеток,
-                    for (int sy = -1; sy <= +1; sy ++)
+        for (int x = 0; x < Config.WIDTH; x++) //суть: выбираем конкретную клетку на экране,
+            for (int y = 0; y < Config.HEIGHT; y++) {
+                for (int sx = -1; sx <= +1; sx++)  //вокруг этой клетки проверяем 8 клеток,
+                    for (int sy = -1; sy <= +1; sy++)
                         if (!(sx == 0) && (sy == 0))
-                            boxes[x][y].cell.addNear(boxes[  //каждую клетку вокруг добавляяем в качестве соседа
+                            boxes[x][y].cell.addNear(boxes[  //каждую клетку вокруг добавляем в качестве соседа
                                     (x + sx + Config.WIDTH) % Config.WIDTH]
                                     [(y + sy + Config.HEIGHT) % Config.HEIGHT].cell);
-                for (x = 10; x < 15; x ++)
-                boxes[x][10].cell.status = Status.LIVE;
+                for (x = 10; x < 15; x++)
+                    boxes[x][10].cell.status = Status.LIVE;
                 boxes[x][10].setColor();
             }
 
     }
 
     private void initTimer() {
+        TimerListener t1 = new TimerListener();
+        Timer timer = new Timer(Config.SLEEPMS, t1);
+        timer.start();
+    }
 
+    private class TimerListener implements ActionListener {
+
+        boolean flop = false;
+
+        @Override
+        public void actionPerformed(ActionEvent e) { //вызываем для каждой ячейки следующий этап
+            flop = !flop;
+            for (int x = 0; x < Config.WIDTH; x++)
+                for (int y = 0; y < Config.HEIGHT; y++) {
+                    if (flop)
+                    boxes[x][y].step1();
+                    else
+                    boxes[x][y].step2();
+
+                }
+        }
     }
 }
