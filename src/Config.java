c0995bab22b869a65cae1f36b38eae23d98be9e6
Ksenia <ksyshishe@mail.ru>
2023-12-26 22:59:22 +0300

    Initial commit

---
+++ src/Config.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Config {
+    public static final int SIZE = 20;
+    public static final int WIDTH = 40;
+    public static final int HEIGHT = 30;
+
+    public static Color getColor (Status status){
+        return switch (status) {
+            default -> Color.BLACK;
+            case BORN -> Color.GRAY;
+            case LIVE -> Color.DARK_GRAY;
+            case DIED -> Color.BLUE;
+        };
+    }
+}
---
+++ src/Window.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+
+public class Window implements Runnable {
+
+    JFrame frame;
+    Box[][] boxes;
+
+    @Override
+    public void run() {
+        initFrame();
+        initBoxes();
+        initTimer();
+    }
+
+    private void initFrame() {
+        frame = new JFrame();
+        frame.getContentPane().setLayout(null);
+        frame.setSize(Config.SIZE * Config.WIDTH, Config.HEIGHT * Config.HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setTitle("Game of life");
+    }
+
+    void initBoxes() {
+        boxes = new Box[Config.WIDTH][Config.HEIGHT];
+        for (int x = 0; x < Config.WIDTH; x ++)
+            for (int y = 0; y < Config.HEIGHT; y ++){
+                boxes [x][y] = new Box (x,y);
+                frame.add(boxes[x][y]);
+            }
+
+        for (int x = 0; x < Config.WIDTH; x ++) //суть: выбираем конкретную клетку на экране,
+            for (int y = 0; y < Config.HEIGHT; y ++){
+                for (int sx = -1; sx <= +1; sx ++)  //вокруг этой клетки проверяем 8 клеток,
+                    for (int sy = -1; sy <= +1; sy ++)
+                        if (!(sx == 0) && (sy == 0))
+                            boxes[x][y].cell.addNear(boxes[  //каждую клетку вокруг добавляяем в качестве соседа
+                                    (x + sx + Config.WIDTH) % Config.WIDTH]
+                                    [(y + sy + Config.HEIGHT) % Config.HEIGHT].cell);
+                for (x = 10; x < 15; x ++)
+                boxes[x][10].cell.status = Status.LIVE;
+                boxes[x][10].setColor();
+            }
+
+    }
+
+    private void initTimer() {
+
+    }
+}
